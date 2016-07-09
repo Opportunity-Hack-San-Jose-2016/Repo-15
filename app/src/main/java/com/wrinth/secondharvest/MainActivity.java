1c19d254fca59a82b312a6efc45fb7ee44ff2aba
Wrinth <johnl4112@gmail.com>
2016-07-09 16:07:14 -0700

    changed manifest

--- app/src/main/java/com/wrinth/secondharvest/MainActivity.java
+++ app/src/main/java/com/wrinth/secondharvest/MainActivity.java
@@ -1,14 +1,48 @@
 package com.wrinth.secondharvest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button eventButton, memberButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("Main Screen");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eventButton = (Button) findViewById(R.id.event_button);
+        // Set a click listener on that View
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the New Event Button is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NewEventActivity}
+                Intent eventIntent = new Intent(MainActivity.this, EventActivity.class);
+
+                // Start the NewEEventActivity
+                startActivity(eventIntent);
+            }
+        });
+
+        memberButton = (Button) findViewById(R.id.events_list_button);
+        // Set a click listener on that View
+        memberButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the Events List Button is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link EventsListActivity}
+                Intent memberIntent = new Intent(MainActivity.this, MemberActivity.class);
+
+                // Start the EventsListActivity
+                startActivity(memberIntent);
+            }
+        });
+
+
     }
 }
