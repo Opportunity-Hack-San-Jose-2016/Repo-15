4d8218e5def38596421be0f8b6ee9e5c3e6f5169
Wrinth <johnl4112@gmail.com>
2016-07-10 14:01:50 -0700

    temp

--- app/src/main/java/com/wrinth/secondharvest/EventCheckInActivity.java
+++ app/src/main/java/com/wrinth/secondharvest/EventCheckInActivity.java
@@ -1,7 +1,18 @@
 package com.wrinth.secondharvest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class EventCheckInActivity extends AppCompatActivity {
 
@@ -9,5 +20,32 @@ public class EventCheckInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_check_in);
+
+
+        final ArrayList<Member> memberArray = new ArrayList<Member>();
+        memberArray.add(new Member("2","Tom Han", "M", "San Jose", 1234567890));
+        memberArray.add(new Member("12", "Jerry Queen", "M", "Santa Clara", 1234567891));
+        memberArray.add(new Member("13", "John Whi", "M", "San Jose", 1234567890));
+        memberArray.add(new Member("16", "Jerry Queen", "M", "Santa Clara", 1234567891));
+
+        MemberAdapter itemAdapter =
+                new MemberAdapter(this, memberArray);
+
+        ListView listView = (ListView) findViewById(R.id.eventCheckInView);
+
+        listView.setAdapter(itemAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Member member = memberArray.get(position);
+                String memberID = member.getMemberID();
+
+                Toast.makeText(EventCheckInActivity.this, "User Checked!", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/wrinth/secondharvest/MemberListActivity.java
+++ app/src/main/java/com/wrinth/secondharvest/MemberListActivity.java
@@ -2,8 +2,6 @@ package com.wrinth.secondharvest;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
