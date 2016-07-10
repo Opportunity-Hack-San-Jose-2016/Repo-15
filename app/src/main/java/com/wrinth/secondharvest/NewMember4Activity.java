4e80979dd683cb1536a11890781a5beb7d7bf424
Mitesh Jain <mitjain@paypal.com>
2016-07-10 11:07:17 -0700

    Member edit

--- app/src/main/java/com/wrinth/secondharvest/NewMember4Activity.java
+++ app/src/main/java/com/wrinth/secondharvest/NewMember4Activity.java
@@ -10,9 +10,8 @@ import android.widget.Button;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-
-
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class NewMember4Activity extends AppCompatActivity {
@@ -21,43 +20,47 @@ public class NewMember4Activity extends AppCompatActivity {
 
 
 
-
+    JSONObject obj;
     private Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
-
-
-
+        Bundle extraEventID = getIntent().getExtras();
+        if (extraEventID == null) {
+            obj = null;
+        }
+        try {
+            obj = new JSONObject(extraEventID.getString("obj"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         setContentView(R.layout.activity_new_member4);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         done = (Button) findViewById(R.id.done);
 
-
-
-
-
-
-
-
-
-
         done.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
+
+                try {
+                    obj.put("ethnicity","White");
+                    obj.put("disability","1");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference();
 
+                System.out.println(obj);
 
                 User user = new User("chun", "ha", "awerew", "awerew", "asdfasdfasd", "werwer", "werwer","werwer","werwer","werwer", "werwer");
-
 
                 myRef.child("user_detail").setValue(user);
 
