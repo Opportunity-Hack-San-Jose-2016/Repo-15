1f5f8a836927cc911431439f3a8dcda40d1332b6
Mitesh Jain <mitjain@paypal.com>
2016-07-09 19:57:06 -0700

    New event

--- app/src/main/java/com/wrinth/secondharvest/NewEventActivity.java
+++ app/src/main/java/com/wrinth/secondharvest/NewEventActivity.java
@@ -1,13 +1,118 @@
 package com.wrinth.secondharvest;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class NewEventActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class NewEventActivity extends AppCompatActivity   {
+
+    private TextView date_text_view;
+    private Button create_button;
+    private EditText location_text;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private String location;
+
+
+    static final int DATE_PICKER_ID = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        date_text_view = (TextView) findViewById(R.id.text_date);
+        location_text = (EditText)findViewById(R.id.location_text);
+        create_button = (Button) findViewById(R.id.create_button);
+
+        // Get current date by calender
+
+        final Calendar c = Calendar.getInstance();
+        year  = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day   = c.get(Calendar.DAY_OF_MONTH);
+        // Show current date
+
+        date_text_view.setText(new StringBuilder()
+                // Month is 0 based, just add 1
+                .append(month + 1).append("-").append(day).append("-")
+                .append(year).append(" "));
+
+        // Button listener to show date picker dialog
+
+        date_text_view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // On button click show datepicker dialog
+                showDialog(DATE_PICKER_ID);
+
+            }
+
+        });
+
+
+        create_button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                System.out.println("================");
+                System.out.println(year);
+                System.out.println(month);
+                System.out.println(day);
+                location = location_text.getText().toString();
+                System.out.println(location);
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_PICKER_ID:
+
+                // open datepicker dialog.
+                // set date picker for current date
+                // add pickerListener listner to date picker
+                return new DatePickerDialog(this, pickerListener, year, month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        @Override
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+
+            year  = selectedYear;
+            month = selectedMonth;
+            day   = selectedDay;
+
+            // Show selected date
+            date_text_view.setText(new StringBuilder().append(month + 1)
+                    .append("-").append(day).append("-").append(year)
+                    .append(" "));
+
+        }
+    };
+
 }
