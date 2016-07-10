fad39e359bc58760a18db1b318bbcba90bb8d97d
c33wang@gmail.com <tempalr011>
2016-07-10 12:11:55 -0700

    Merge branch 'master' of /Users/vm/Desktop/this/Repo-15 with conflicts.

--- app/src/main/java/com/wrinth/secondharvest/NewMember4Activity.java
+++ app/src/main/java/com/wrinth/secondharvest/NewMember4Activity.java
@@ -7,65 +7,83 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class NewMember4Activity extends AppCompatActivity {
 
 
 
-
-
-    JSONObject obj;
     private Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Bundle extraEventID = getIntent().getExtras();
-        if (extraEventID == null) {
-            obj = null;
-        }
-        try {
-            obj = new JSONObject(extraEventID.getString("obj"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
         setContentView(R.layout.activity_new_member4);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         done = (Button) findViewById(R.id.done);
 
+
+
+
         done.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-
-                Intent memberIntent = new Intent(NewMember4Activity.this, MemberListActivity.class);
-
-                try {
-                    obj.put("ethnicity","White");
-                    obj.put("disability","1");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
+                database.setPersistenceEnabled(true);
                 DatabaseReference myRef = database.getReference();
 
 
-                System.out.println(obj);
-
                 User user = new User("chun", "ha", "awerew", "awerew", "asdfasdfasd", "werwer", "werwer","werwer","werwer","werwer", "werwer");
+                myRef.child("user_detail").child("0").setValue(user);
+                myRef.child("user_detail").child("1").setValue(user);
+                myRef.child("user_detail").child("2").setValue(user);
+                myRef.child("user_detail").child("3").setValue(user);
+                myRef.child("user_detail").child("4").setValue(user);
 
-                myRef.child("user_detail").setValue(user);
 
+
+
+
+
+
+
+
+                myRef.child("user_detail").addListenerForSingleValueEvent(
+                        new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                long totaluser= dataSnapshot.getChildrenCount();
+                                System.out.println(totaluser);
+
+
+
+
+                            }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                            }
+                        });
+
+
+
+
+
+
+
+
+
+
+                Intent memberIntent = new Intent(NewMember4Activity.this, MemberActivity.class);
                 startActivity(memberIntent);
 
 
