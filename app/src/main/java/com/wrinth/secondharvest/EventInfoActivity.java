35053c149b912f90a6c75a636b4667638cdb2731
Mitesh Jain <mitjain@paypal.com>
2016-07-10 13:49:50 -0700

    Statistics

--- app/src/main/java/com/wrinth/secondharvest/EventInfoActivity.java
+++ app/src/main/java/com/wrinth/secondharvest/EventInfoActivity.java
@@ -1,8 +1,11 @@
 package com.wrinth.secondharvest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,6 +13,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class EventInfoActivity extends AppCompatActivity {
 
 
+    Button statisics;
+    Button notification;
+    Button checkin;
     String eventID;
 
     @Override
@@ -17,16 +23,31 @@ public class EventInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
 
-        Bundle extraEventID = getIntent().getExtras();
-        if (extraEventID == null) {
-            eventID = "0";
-        }
-        eventID = extraEventID.getString("eventID");
+//        Bundle extraEventID = getIntent().getExtras();
+//        if (extraEventID == null) {
+//            eventID = "0";
+//        }
+//        eventID = extraEventID.getString("eventID");
+
+        statisics = (Button) findViewById(R.id.statistic_button);
+        notification = (Button) findViewById(R.id.remainder_button);
+        checkin = (Button) findViewById(R.id.check_in_button);
+
 
 //        FirebaseDatabase database = FirebaseDatabase.getInstance();
 //        DatabaseReference myRef = database.getReference();
 //
 //        myRef.setValue("Hello, World!asdfasdfasdfasdfasdfsadfsdfdsf");
+        statisics.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the Events List Button is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link EventsListActivity}
+                Intent memberIntent = new Intent(EventInfoActivity.this, StatisticsActivity.class);
+                // Start the EventsListActivity
+                startActivity(memberIntent);
+            }
+        });
 
     }
 }
